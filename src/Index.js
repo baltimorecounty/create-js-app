0bfac79a5e09cb4718c7bbeeae92df413f9c346e
Marty Powell <jpowel25@gmail.com>
2019-09-23 21:09:53 -0400

    added test examples

--- src/Index.js
+++ src/Index.js
@@ -1,7 +1,23 @@
 import "./Index.scss";
 
-const Init = message => {
+const handleChangeTitleClick = clickEvent => {
+  const pageTitle = document.querySelectorAll("h1");
+
+  console.log("got here", pageTitle);
+
+  if (pageTitle) {
+    pageTitle[0].textContent = "My Really Awesome App";
+  }
+};
+
+const init = message => {
   console.log(message);
 };
 
-Init("Welcome to my app.");
+console.log("test", document.body.innerHTML);
+
+document
+  .getElementById("change-title")
+  .addEventListener("click", handleChangeTitleClick);
+
+init("Welcome to my app.");
--- src/Index.test.js
+++ src/Index.test.js
@@ -1,7 +1,36 @@
-// import { sum } from "./Index";
+import { getByText } from "@testing-library/dom";
 
-// test("this test should be replaced with an actual test", () => {
-//   const actualSum = sum(1, 2);
+global.console = { log: jest.fn() };
 
-//   expect(actualSum).toEqual(4);
-// });
+beforeEach(() => {
+  createAppContainer();
+  require("./Index.js");
+});
+
+afterEach(() => {
+  document.body.firstChild.remove();
+  jest.resetModules();
+});
+
+const createAppContainer = () => {
+  document.body.innerHTML = `<div id="app">
+      <h1>My Awesome App</h1>
+      <button id="change-title" type="button">Change Page Title</button>
+    </div>`;
+};
+
+test("this test should be replaced with an actual jest test", () => {
+  expect(console.log).toBeCalled();
+});
+
+test("this test should can be replace with an actual dom testing-library test", () => {
+  const appContainer = document.getElementById("app");
+  const changeButton = getByText(appContainer, "Change Page Title");
+  const pageHeading = getByText(appContainer, "My Awesome App");
+
+  expect(pageHeading.textContent).toEqual("My Awesome App");
+
+  changeButton.click();
+
+  expect(pageHeading.textContent).toEqual("My Really Awesome App");
+});
